5880fae3a44e6d3d460a07329736c232b215da47
Anthony PV <68924473+Holkeano526@users.noreply.github.com>
2026-03-29 16:02:20 -0500

    Update google.js

--- api/google.js
+++ api/google.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  // 🚨 PEGA AQUÍ LA URL DE TU "NUEVA IMPLEMENTACIÓN" DE APPS SCRIPT
+
   const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyI6gcaiR4mEKGP594j3meC4QFiaQcgvT-3Bq5djbA0k69P3FgwoWSvff5plzQv8icM/exec";
 
   // Solo aceptamos peticiones POST
@@ -8,15 +8,14 @@ export default async function handler(req, res) {
   }
 
   try {
-    // El servidor de Vercel hace la petición a Google (Adiós CORS)
+
     const googleResponse = await fetch(GOOGLE_SCRIPT_URL, {
       method: 'POST',
       body: JSON.stringify(req.body)
     });
 
     const data = await googleResponse.json();
-    
-    // Devolvemos la respuesta de Google a nuestra página web
+
     res.status(200).json(data);
     
   } catch (error) {
